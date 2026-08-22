37555a282f7e00ecb0a5b4038eccf0eeec6a2df0
hsrokon <hsrokonofficial@gmail.com>
2026-08-22 12:46:39 +0600

    made a pull req, and some change

--- Lab Class 6 Lab Sheet 4/NestedConvolution.m
+++ Lab Class 6 Lab Sheet 4/NestedConvolution.m
@@ -5,6 +5,7 @@ x2=1; y2=1;
 x3=2; y3=0;
 x4=3; y4=-1;
  
+%... means continuous operator
 p1=y1 *conv([1 -x2], conv([1 -x3],[1 -x4]) )...
             /((x1-x2)*(x1-x3)*(x1-x4));
 p2=y2 *conv([1 -x1], conv([1 -x3],[1 -x4]) )...
@@ -15,5 +16,4 @@ p4=y4 *conv([1 -x1], conv([1 -x2],[1 -x3]) )...
             /((x4-x1)*(x4-x2)*(x4-x3));
  
 p=p1+p2+p3+p4
-polyval(p,2)
-
+polyval(p,2);
